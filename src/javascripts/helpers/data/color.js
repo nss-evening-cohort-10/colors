1752abf533582b49ae888af4dbf41a5f222ef9f6
Zoe Ames <zoeames@gmail.com>
2019-10-14 19:04:01 -0500

    added colors data

---
+++ src/javascripts/helpers/data/color.js
@@ -0,0 +1,42 @@
+const colors = [
+  {
+    id: 'color1',
+    name: 'red',
+    hexValue: 'FF0000',
+    upVotes: 15,
+  },
+  {
+    id: 'color2',
+    name: 'orange',
+    hexValue: 'FF7F00',
+    upVotes: 3,
+  },
+  {
+    id: 'color3',
+    name: 'yellow',
+    hexValue: 'FFFF00',
+    upVotes: 2,
+  },
+  {
+    id: 'color4',
+    name: 'green',
+    hexValue: '00FF00',
+    upVotes: 6,
+  },
+  {
+    id: 'color5',
+    name: 'blue',
+    hexValue: '0000FF',
+    upVotes: 25,
+  },
+  {
+    id: 'color6',
+    name: 'violet',
+    hexValue: '9400D3',
+    upVotes: 11,
+  },
+];
+
+const getColors = () => colors;
+
+export default { getColors };
